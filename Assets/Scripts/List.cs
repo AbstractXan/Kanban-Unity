81aae9ca3af2478c5d0086eee39e305faf574aa7
Priyansh Sangule <psangule@schoology.com>
2020-10-27 11:45:00 +0530

    Fixed List

---
+++ Assets/Scripts/List.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class List : MonoBehaviour
+{
+    public string title = "List";
+
+    void Update(){
+        transform.Find("Text").gameObject.GetComponent<Text>().text = title;
+    }
+}
